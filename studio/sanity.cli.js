8670f3c66a05b8e9122ac14e129d960ffcf81192
creatorsnetwork-io <hello@creatorsnetwork.io>
2026-07-14 16:31:20 +0400

    Update projectId in sanity.cli.js

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -3,7 +3,7 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     // Replace YOUR_PROJECT_ID with the Project ID from sanity.io/manage
-    projectId: 'YOUR_PROJECT_ID',
+    projectId: 'yodi9k1k',
     dataset: 'production',
   },
 })
